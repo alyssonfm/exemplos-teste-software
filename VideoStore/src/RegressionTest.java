bf5efd5a535ac0ada6a760e1906465c2078b04bf
alyssonfm <alyssonfilgueira@gmail.com>
2022-06-09 11:34:21 -0300

    Exemplos acerca de Refactoring

---
+++ VideoStore/src/RegressionTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class, RegressionTest01.class })
+public class RegressionTest {
+}
+
